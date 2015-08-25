2d674ca91ef9ba3ae805a23d5445373235faecb4
kaushikmukunda <kmukunda@qualcomm.com>
2015-08-25 13:36:09 -0700

    Use cursor adapter

--- app/src/main/java/qlearn/qualcomm/com/contentprovider/MainActivity.java
+++ app/src/main/java/qlearn/qualcomm/com/contentprovider/MainActivity.java
@@ -2,21 +2,22 @@ package qlearn.qualcomm.com.contentprovider;
 
 import android.content.ContentResolver;
 import android.database.Cursor;
-import android.provider.UserDictionary;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.UserDictionary;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
-    TextView dTv;
+    ListView dTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dTv = (TextView) findViewById(R.id.dictionary_tv);
+        dTv = (ListView) findViewById(R.id.dictionary_tv);
         printCustomDictionary();
     }
 
@@ -24,21 +25,17 @@ public class MainActivity extends ActionBarActivity {
         ContentResolver contentResolver = getContentResolver();
         Cursor cursor = contentResolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
 
-        int idCol = cursor.getColumnIndex(UserDictionary.Words._ID);
-        int freqCol = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
-        int wordCol = cursor.getColumnIndex(UserDictionary.Words.WORD);
-        StringBuffer res = new StringBuffer();
+        String[] cols = {UserDictionary.Words.WORD,  UserDictionary.Words.FREQUENCY};
+        int[] viewIds = {android.R.id.text1, android.R.id.text2};
 
-        while (cursor.moveToNext()) {
-            StringBuffer sb = new StringBuffer();
-            sb.append(cursor.getInt(idCol) + " " +
-                      cursor.getInt(freqCol) + " " +
-                      cursor.getString(wordCol) + "\n");
-            res.append(sb.toString());
-        }
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+            android.R.layout.two_line_list_item,
+            cursor,
+            cols,
+            viewIds,
+            0);
 
-        cursor.close();
-        dTv.setText(res.toString());
+        dTv.setAdapter(adapter);
     }
 
 }
